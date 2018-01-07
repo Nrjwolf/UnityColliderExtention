b6bcfcf870b87299601d21d95278049faeee4514
Alexander <Nrjwolf@MacBook-Pro-Alexander.local>
2018-01-08 01:57:55 +0300

    init

---
+++ ColliderExtention.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ColliderExtention
+{
+
+    //--- Collision2D ---//
+
+    public static void OnCollisionEnter2D(this Collision2D collision, Action<Collision2D> callback)
+    {
+        CheckColliderListenerComponent(collision.collider).onCollisionEnter2D += callback;
+    }
+
+    public static void OnCollisionStay2D(this Collision2D collision, Action<Collision2D> callback)
+    {
+        CheckColliderListenerComponent(collision.collider).onCollisionStay2D += callback;
+    }
+
+    public static void OnCollisionExit2D(this Collision2D collision, Action<Collision2D> callback)
+    {
+        CheckColliderListenerComponent(collision.collider).onCollisionExit2D += callback;
+    }
+
+    //--- Trigger2D ---//
+
+    public static void OnTriggerStay2D(this Collider2D collider, Action<Collider2D> callback)
+    {
+        CheckColliderListenerComponent(collider).onTriggerStay2D += callback;
+    }
+
+    public static void OnTriggerEnter2D(this Collider2D collider, Action<Collider2D> callback)
+    {
+        CheckColliderListenerComponent(collider).onTriggerEnter2D += callback;
+    }
+
+    public static void OnTriggerExit2D(this Collider2D collider, Action<Collider2D> callback)
+    {
+        CheckColliderListenerComponent(collider).onTriggerExit2D += callback;
+    }
+
+    /// Добавляет ColliderListener, если его нет
+    private static ColliderListener CheckColliderListenerComponent(Collider2D collider)
+    {
+        if (collider.GetComponent<ColliderListener>() == null)
+            return collider.gameObject.AddComponent<ColliderListener>();
+        return collider.gameObject.GetComponent<ColliderListener>();
+    }
+}
---
+++ ColliderListener.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+public class ColliderListener : MonoBehaviour
+{
+    public Action<Collision2D> onCollisionEnter2D;
+    public Action<Collision2D> onCollisionStay2D;
+    public Action<Collision2D> onCollisionExit2D;
+
+    public Action<Collider2D> onTriggerEnter2D;
+    public Action<Collider2D> onTriggerStay2D;
+    public Action<Collider2D> onTriggerExit2D;
+
+    //--- Collision2D ---//
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        onCollisionEnter2D(collision);
+    }
+
+    public void OnCollisionStay2D(Collision2D collision)
+    {
+        onCollisionStay2D(collision);
+    }
+
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        onCollisionExit2D(collision);
+    }
+
+    //--- Trigger2D ---//
+
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        onTriggerEnter2D(collider);
+    }
+
+    public void OnTriggerStay2D(Collider2D collider)
+    {
+        onTriggerEnter2D(collider);
+    }
+
+    public void OnTriggerExit2D(Collider2D collider)
+    {
+        onTriggerExit2D(collider);
+    }
+}
